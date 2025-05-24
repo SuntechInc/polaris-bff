cf274ae51063fc152e646c211e183463e44b9f14
lucas.previato <lucaspreviato@live.com>
2025-05-23 21:44:41 -0300

    fix: ci/cd

--- src/gateway.module.ts
+++ src/gateway.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GatewayController } from './gateway.controller';
 import { BffModule } from './routes/bff.module';
+import { HealthController } from './healthz/healthz.controller';
 
 @Module({
   imports: [ BffModule],
-  controllers: [GatewayController, ],
+  controllers: [GatewayController, HealthController],
   providers: [],
 })
 export class GatewayModule {}

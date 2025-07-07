2d87d37b18505c41c634292100de9c7ebcdfd8f8
lucas.previato <lucaspreviato@live.com>
2025-07-07 19:53:57 -0300

    fix: improv logs in api

--- src/gateway.module.ts
+++ src/gateway.module.ts
@@ -7,6 +7,8 @@ import { JwtModule } from '@nestjs/jwt';
 import { getJwtConfig } from './config/jwt.config';
 import { JwtAuthGuard } from './guards/jwt.guard';
 import { AdminGuard } from './guards/admin.guard';
+import { LoggerModule } from 'nestjs-pino';
+import { CustomLogger } from './custom.logger';
 
 @Module({
   imports: [
@@ -14,6 +16,11 @@ import { AdminGuard } from './guards/admin.guard';
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'local' ? '.env.local' : undefined,
       ignoreEnvFile: process.env.NODE_ENV === 'production',
+    }),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        level: 'trace'
+      }
     }),
     HttpModule,
     JwtModule.registerAsync({
@@ -23,6 +30,7 @@ import { AdminGuard } from './guards/admin.guard';
     }),
     BffModule],
   controllers: [HealthController],
-  providers: [JwtAuthGuard, AdminGuard],
+  providers: [JwtAuthGuard, AdminGuard, CustomLogger],
+  exports: [CustomLogger],
 })
 export class GatewayModule {}
